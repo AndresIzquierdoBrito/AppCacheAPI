4472203766349ec88658dc45ed25b8c7ee769bf0
andresizbri <andres.izbri@gmail.com>
2024-05-13 18:44:30 +0200

    Feat: creat constants class
    - added a constants static class for max ideas, ideas per category and max categories per user

---
+++ AppCacheAPI/Utilities/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace AppCacheAPI.Utilities
+{
+    public static class Constants
+    {
+        public const int MaxIdeasPerUser = 100; 
+
+        public const int MaxIdeasPerCategory = 100;
+
+        public const int MaxCategoriesPerUser = 10;
+
+    }
+}
