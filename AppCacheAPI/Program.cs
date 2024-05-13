b7ced9e13d609490d6542a492806b544799b309c
andresizbri <andres.izbri@gmail.com>
2024-05-13 12:45:06 +0200

    BREAKING CHANGE: added database context
    - removed base files of project creation
    - reverse engineer db context and idea base model

--- AppCacheAPI/Program.cs
+++ AppCacheAPI/Program.cs
@@ -1,9 +1,19 @@
+using AppCacheAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
 
 // Add services to the container.
 
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+services.AddDbContext<AppCacheDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
